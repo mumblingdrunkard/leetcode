b84d06e91ed293086dd95a71a22a5bcee6988a28
mumblingdrunkard <mumblingdrunkard@protonmail.com>
2022-07-18 15:22:28 +0200

    Add comments and clarify

--- median-of-two-sorted-arrays-rs/src/main.rs
+++ median-of-two-sorted-arrays-rs/src/main.rs
@@ -3,6 +3,12 @@ struct Solution {}
 
 #[allow(unused)]
 impl Solution {
+    // idea: find splits i and j in nums1 and nums2 such that
+    // a) i + j = (nums1.len() + nums2.len()) / 2, and
+    // b) nums1[i-1] <= nums2[j] && nums2[j-1] <= nums1[i]
+    //
+    // These two conditions will find the mid-point of the merged array
+    // To achieve the condition of O(log(n + m)) we can binary search for this split point.
     pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
         use std::cmp;
 
@@ -16,33 +22,38 @@ impl Solution {
 
         // start with all elements from a (the shorter array)
         let mut i = a.len();
-        let mut step = (half / 2).clamp(1, usize::MAX);
+        let mut step = (half / 2).clamp(1, usize::MAX); // binary search, step can't be less than 1
 
         loop {
-            // supplement with elements from b (the longer array)
+            // supplement with elements from b (the longer array) until we have half the elements
             let mut j = half - i;
 
+            // initialise al, ar, bl, br with default values
             let (mut al, mut ar, mut bl, mut br) = (i32::MIN, i32::MAX, i32::MIN, i32::MAX);
+            // fill values if possible
+            // condition.then(|| something); is functionally equivalent to
+            // condition && { something; true/false };
             (i > 0).then(|| al = a[i - 1]);
             (i < a.len()).then(|| ar = a[i]);
             (j > 0).then(|| bl = b[j - 1]);
             (j < b.len()).then(|| br = b[j]);
 
-            println!("(i, j) = {:?}", (i, j));
-            println!("(al, ar, bl, br) = {:?}", (al, ar, bl, br));
-
+            // we found the mid-point
             if al <= br && bl <= ar {
                 return if length % 2 == 0 {
                     (cmp::max(al, bl) + cmp::min(ar, br)) as f64 / 2.0
                 } else {
                     cmp::min(ar, br) as f64
                 };
-            } else if al < br {
+            } else if bl > ar {
+                // split point in a is too far left
                 i = (i + step).clamp(0, a.len());
             } else {
+                // split point in a is too far right
                 i = (i - step).clamp(0, a.len());
             }
 
+            // halve step
             step = (step / 2).clamp(1, usize::MAX);
         }
     }
