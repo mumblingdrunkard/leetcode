d97daf98192917b5257a176934c97648682356b2
mumblingdrunkard <mumblingdrunkard@protonmail.com>
2022-07-30 03:24:39 +0200

    Add solution for daily problem "word-subsets"

---
+++ word-subsets-rs/src/main.rs
@@ -0,0 +1,57 @@
+struct Solution {}
+impl Solution {
+    pub fn word_subsets(words1: Vec<String>, words2: Vec<String>) -> Vec<String> {
+        use std::collections::HashMap;
+        // construct the universal word by
+        let universal = words2.iter().fold(HashMap::new(), |mut u, s| {
+            // taking the count of each character of each word
+            let v = s.chars().fold(HashMap::new(), |mut v, c| {
+                let count = v.entry(c).or_insert(0usize);
+                *count += 1;
+                v
+            });
+
+            // and finding the maximum occurence of each character
+            for (c, count) in v {
+                let current = u.entry(c).or_insert(0);
+                *current = std::cmp::max(*current, count);
+            }
+
+            u
+        });
+
+        // for each word
+        words1
+            .into_iter()
+            .filter(|s| {
+                // count occurences of each character in the word
+                let v = s.chars().fold(HashMap::new(), |mut v, c| {
+                    let count = v.entry(c).or_insert(0usize);
+                    *count += 1;
+                    v
+                });
+
+                // ensure that the word has at least the characters in the universal word
+                universal
+                    .iter()
+                    .all(|(c, count)| v.get(c).unwrap_or(&0) >= count)
+            })
+            .collect()
+    }
+}
+
+fn main() {
+    println!(
+        "{:?}",
+        Solution::word_subsets(
+            vec![
+                "amazon".to_string(),
+                "apple".to_string(),
+                "facebook".to_string(),
+                "google".to_string(),
+                "leetcode".to_string(),
+            ],
+            vec!["e".to_string(), "o".to_string(),]
+        )
+    );
+}
