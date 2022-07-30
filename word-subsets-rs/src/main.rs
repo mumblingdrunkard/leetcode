091904f320ee05503dc311eefdd2cd39edd54825
mumblingdrunkard <mumblingdrunkard@protonmail.com>
2022-07-30 04:13:06 +0200

    Add licence

--- word-subsets-rs/src/main.rs
+++ word-subsets-rs/src/main.rs
@@ -1,4 +1,16 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// This Source Code Form is "Incompatible With Secondary Licenses", as
+// defined by the Mozilla Public License, v. 2.0.
+//
+// Copyright © 2022 mumblingdrunkard
+//
+// solution for problem found at https://leetcode.com/problems/word-subsets/
+
 struct Solution {}
+
 impl Solution {
     pub fn word_subsets(words1: Vec<String>, words2: Vec<String>) -> Vec<String> {
         use std::collections::HashMap;
