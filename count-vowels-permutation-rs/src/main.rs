b75aae8b11514f59faec0f84a00e6f219fc67bbb
mumblingdrunkard <mumblingdrunkard@protonmail.com>
2022-08-07 23:22:51 +0200

    Add logn solution for "count-vowels-permutation"

--- count-vowels-permutation-rs/src/main.rs
+++ count-vowels-permutation-rs/src/main.rs
@@ -13,6 +13,11 @@ struct Solution {}
 
 impl Solution {
     pub fn count_vowel_permutation(n: i32) -> i32 {
+        Self::count_vowel_permutation_log(n as usize) as i32
+    }
+
+    #[allow(unused)]
+    pub fn count_vowel_permutation_linear(n: i32) -> i32 {
         // 0 -> 'a'
         // 1 -> 'e'
         // 2 -> 'i'
@@ -24,6 +29,7 @@ impl Solution {
         // 2 can follow { 1, 3 }
         // 3 can follow { 2 }
         // 4 can follow { 2, 3 }
+        //
         const M: i32 = 1000000007;
         let n = n as usize;
 
@@ -39,6 +45,87 @@ impl Solution {
 
         // this is ducking fugly
         (((p.0 + p.1) % M + (p.2 + p.3) % M) % M + p.4) % M
+    }
+
+    #[allow(unused)]
+    fn log2(n: usize) -> usize {
+        let mut log = 1;
+        let mut k = 1;
+        while k < n {
+            log += 1;
+            k *= 2;
+        }
+        log
+    }
+
+    #[allow(unused)]
+    pub fn count_vowel_permutation_log(n: usize) -> usize {
+        const M: usize = 1000000007;
+        let follows = vec![
+            vec![1usize, 2, 4],
+            vec![0, 2],
+            vec![1, 3],
+            vec![2],
+            vec![2, 3],
+        ];
+
+        let mut p: Vec<[[usize; 5]; 5]> = vec![[[0; 5]; 5]];
+
+        p[0][0][0] = 1;
+        p[0][1][1] = 1;
+        p[0][2][2] = 1;
+        p[0][3][3] = 1;
+        p[0][4][4] = 1;
+
+        for l in 1..Self::log2(n) {
+            // 0 can be followed by { 1 }
+            // 1 can be followed by { 0, 2 }
+            // 2 can be followed by { 0, 3, 4 }
+            // 3 can be followed by { 2, 4 }
+            // 4 can be followed by { 0 }
+            let mut next = [[0; 5]; 5];
+            for i in 0..5 {
+                for j in 0..5 {
+                    let mut sum = 0;
+                    for a in 0..5 {
+                        for &b in &follows[a] {
+                            sum += (p[l - 1][i][a] * p[l - 1][b][j]) % M;
+                            sum %= M;
+                        }
+                    }
+                    next[i][j] = sum;
+                }
+            }
+            p.push(next);
+        }
+
+        let mut iter = p
+            .into_iter()
+            .enumerate()
+            .filter_map(|(i, r)| ((n >> i) & 1 == 1).then(|| r));
+
+        let first = iter.next().unwrap();
+        let res = iter.fold(first, |c, r| {
+            let mut next = [[0; 5]; 5];
+            for i in 0..5 {
+                for j in 0..5 {
+                    let mut sum = 0;
+                    for a in 0..5 {
+                        for &b in &follows[a] {
+                            sum += (c[i][a] * r[b][j]) % M;
+                            sum %= M;
+                        }
+                    }
+                    next[i][j] = sum;
+                }
+            }
+            next
+        });
+
+        res.into_iter()
+            .map(|r| r.into_iter().sum::<usize>() % M)
+            .sum::<usize>()
+            % M
     }
 }
 
