ec2fd9441458d1f39a1caafc705e276b86207d18
mumblingdrunkard <mumblingdrunkard@protonmail.com>
2022-08-16 04:50:54 +0200

    Add solution for daily problem "word-ladder-ii"

---
+++ word-ladder-ii-rs/src/main.rs
@@ -0,0 +1,185 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// This Source Code Form is "Incompatible With Secondary Licenses", as
+// defined by the Mozilla Public License, v. 2.0.
+//
+// Copyright © 2022 mumblingdrunkard
+//
+// solution for problem found at https://leetcode.com/problems/word-ladder-ii/
+
+struct Solution {}
+
+impl Solution {
+    pub fn find_ladders(
+        begin_word: String,
+        end_word: String,
+        word_list: Vec<String>,
+    ) -> Vec<Vec<String>> {
+        use std::cmp::{min, Reverse};
+        use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
+
+        let mut words = word_list.into_iter().collect::<HashSet<_>>();
+
+        // end_word is not in list => impossible
+        match words.get(&end_word) {
+            None => return vec![],
+            Some(_) => {}
+        }
+
+        // ensure that begin_word is in the list of words
+        words.insert(begin_word.clone());
+
+        // map from words to node indices
+        let map = words
+            .iter()
+            .enumerate()
+            .map(|(i, w)| (w, i))
+            .collect::<HashMap<_, _>>();
+
+        // map from node indices to words
+        let mut rev_map = vec!["".to_string(); words.len()];
+        for (&w, &i) in &map {
+            rev_map[i] = w.clone();
+        }
+
+        // building adjacency lists
+        let mut adj = vec![vec![]; words.len()];
+        let mut shortest = vec![usize::MAX; adj.len()];
+        let mut q = VecDeque::new();
+        let mut visited = HashSet::new();
+        q.push_back(begin_word.clone());
+        while let Some(w) = q.pop_front() {
+            visited.insert(w.clone());
+            for i in 0..w.len() {
+                for c in "abcdefghijklmnopqrstuvwxyz".chars() {
+                    let mut nw = w.chars().collect::<Vec<_>>();
+                    nw[i] = c;
+                    let ns = nw.iter().collect::<String>();
+                    if let Some(_) = words.get(&ns) {
+                        adj[*map.get(&w).unwrap()].push(map.get(&ns).unwrap());
+                        if !visited.contains(&ns) {
+                            visited.insert(ns.clone());
+                            q.push_back(ns.clone());
+                        }
+                    }
+                }
+            }
+        }
+
+        // Dijkstra
+        let mut q = BinaryHeap::new();
+        q.push(Reverse((0, *map.get(&begin_word).unwrap())));
+
+        while let Some(Reverse((l, u))) = q.pop() {
+            for &&v in &adj[u] {
+                shortest[u] = min(l, shortest[u]);
+                if shortest[v] > l + 1 {
+                    q.push(Reverse((l + 1, v)));
+                    shortest[v] = l + 1;
+                }
+            }
+        }
+
+        let mut res = vec![];
+        let mut q = VecDeque::new();
+        q.push_back(vec![*map.get(&end_word).unwrap()]);
+
+        while let Some(path) = q.pop_front() {
+            let u = *path.last().unwrap();
+            let l = shortest[u];
+            if l == 0 {
+                res.push(path);
+                continue;
+            }
+
+            for &&v in &adj[u] {
+                if shortest[v] == l - 1 {
+                    let mut path = path.clone();
+                    path.push(v);
+                    q.push_back(path);
+                }
+            }
+        }
+
+        res.into_iter()
+            .map(|v| {
+                v.into_iter()
+                    .rev()
+                    .map(|u| rev_map[u].clone())
+                    .collect::<Vec<String>>()
+            })
+            .collect()
+    }
+}
+
+fn main() {
+    println!(
+        "{:?}",
+        Solution::find_ladders(
+            "aaaaa".to_string(),
+            "ggggg".to_string(),
+            [
+                "aaaaa", "caaaa", "cbaaa", "daaaa", "dbaaa", "eaaaa", "ebaaa", "faaaa", "fbaaa",
+                "gaaaa", "gbaaa", "haaaa", "hbaaa", "iaaaa", "ibaaa", "jaaaa", "jbaaa", "kaaaa",
+                "kbaaa", "laaaa", "lbaaa", "maaaa", "mbaaa", "naaaa", "nbaaa", "oaaaa", "obaaa",
+                "paaaa", "pbaaa", "bbaaa", "bbcaa", "bbcba", "bbdaa", "bbdba", "bbeaa", "bbeba",
+                "bbfaa", "bbfba", "bbgaa", "bbgba", "bbhaa", "bbhba", "bbiaa", "bbiba", "bbjaa",
+                "bbjba", "bbkaa", "bbkba", "bblaa", "bblba", "bbmaa", "bbmba", "bbnaa", "bbnba",
+                "bboaa", "bboba", "bbpaa", "bbpba", "bbbba", "abbba", "acbba", "dbbba", "dcbba",
+                "ebbba", "ecbba", "fbbba", "fcbba", "gbbba", "gcbba", "hbbba", "hcbba", "ibbba",
+                "icbba", "jbbba", "jcbba", "kbbba", "kcbba", "lbbba", "lcbba", "mbbba", "mcbba",
+                "nbbba", "ncbba", "obbba", "ocbba", "pbbba", "pcbba", "ccbba", "ccaba", "ccaca",
+                "ccdba", "ccdca", "cceba", "cceca", "ccfba", "ccfca", "ccgba", "ccgca", "cchba",
+                "cchca", "cciba", "ccica", "ccjba", "ccjca", "cckba", "cckca", "cclba", "cclca",
+                "ccmba", "ccmca", "ccnba", "ccnca", "ccoba", "ccoca", "ccpba", "ccpca", "cccca",
+                "accca", "adcca", "bccca", "bdcca", "eccca", "edcca", "fccca", "fdcca", "gccca",
+                "gdcca", "hccca", "hdcca", "iccca", "idcca", "jccca", "jdcca", "kccca", "kdcca",
+                "lccca", "ldcca", "mccca", "mdcca", "nccca", "ndcca", "occca", "odcca", "pccca",
+                "pdcca", "ddcca", "ddaca", "ddada", "ddbca", "ddbda", "ddeca", "ddeda", "ddfca",
+                "ddfda", "ddgca", "ddgda", "ddhca", "ddhda", "ddica", "ddida", "ddjca", "ddjda",
+                "ddkca", "ddkda", "ddlca", "ddlda", "ddmca", "ddmda", "ddnca", "ddnda", "ddoca",
+                "ddoda", "ddpca", "ddpda", "dddda", "addda", "aedda", "bddda", "bedda", "cddda",
+                "cedda", "fddda", "fedda", "gddda", "gedda", "hddda", "hedda", "iddda", "iedda",
+                "jddda", "jedda", "kddda", "kedda", "lddda", "ledda", "mddda", "medda", "nddda",
+                "nedda", "oddda", "oedda", "pddda", "pedda", "eedda", "eeada", "eeaea", "eebda",
+                "eebea", "eecda", "eecea", "eefda", "eefea", "eegda", "eegea", "eehda", "eehea",
+                "eeida", "eeiea", "eejda", "eejea", "eekda", "eekea", "eelda", "eelea", "eemda",
+                "eemea", "eenda", "eenea", "eeoda", "eeoea", "eepda", "eepea", "eeeea", "ggggg",
+                "agggg", "ahggg", "bgggg", "bhggg", "cgggg", "chggg", "dgggg", "dhggg", "egggg",
+                "ehggg", "fgggg", "fhggg", "igggg", "ihggg", "jgggg", "jhggg", "kgggg", "khggg",
+                "lgggg", "lhggg", "mgggg", "mhggg", "ngggg", "nhggg", "ogggg", "ohggg", "pgggg",
+                "phggg", "hhggg", "hhagg", "hhahg", "hhbgg", "hhbhg", "hhcgg", "hhchg", "hhdgg",
+                "hhdhg", "hhegg", "hhehg", "hhfgg", "hhfhg", "hhigg", "hhihg", "hhjgg", "hhjhg",
+                "hhkgg", "hhkhg", "hhlgg", "hhlhg", "hhmgg", "hhmhg", "hhngg", "hhnhg", "hhogg",
+                "hhohg", "hhpgg", "hhphg", "hhhhg", "ahhhg", "aihhg", "bhhhg", "bihhg", "chhhg",
+                "cihhg", "dhhhg", "dihhg", "ehhhg", "eihhg", "fhhhg", "fihhg", "ghhhg", "gihhg",
+                "jhhhg", "jihhg", "khhhg", "kihhg", "lhhhg", "lihhg", "mhhhg", "mihhg", "nhhhg",
+                "nihhg", "ohhhg", "oihhg", "phhhg", "pihhg", "iihhg", "iiahg", "iiaig", "iibhg",
+                "iibig", "iichg", "iicig", "iidhg", "iidig", "iiehg", "iieig", "iifhg", "iifig",
+                "iighg", "iigig", "iijhg", "iijig", "iikhg", "iikig", "iilhg", "iilig", "iimhg",
+                "iimig", "iinhg", "iinig", "iiohg", "iioig", "iiphg", "iipig", "iiiig", "aiiig",
+                "ajiig", "biiig", "bjiig", "ciiig", "cjiig", "diiig", "djiig", "eiiig", "ejiig",
+                "fiiig", "fjiig", "giiig", "gjiig", "hiiig", "hjiig", "kiiig", "kjiig", "liiig",
+                "ljiig", "miiig", "mjiig", "niiig", "njiig", "oiiig", "ojiig", "piiig", "pjiig",
+                "jjiig", "jjaig", "jjajg", "jjbig", "jjbjg", "jjcig", "jjcjg", "jjdig", "jjdjg",
+                "jjeig", "jjejg", "jjfig", "jjfjg", "jjgig", "jjgjg", "jjhig", "jjhjg", "jjkig",
+                "jjkjg", "jjlig", "jjljg", "jjmig", "jjmjg", "jjnig", "jjnjg", "jjoig", "jjojg",
+                "jjpig", "jjpjg", "jjjjg", "ajjjg", "akjjg", "bjjjg", "bkjjg", "cjjjg", "ckjjg",
+                "djjjg", "dkjjg", "ejjjg", "ekjjg", "fjjjg", "fkjjg", "gjjjg", "gkjjg", "hjjjg",
+                "hkjjg", "ijjjg", "ikjjg", "ljjjg", "lkjjg", "mjjjg", "mkjjg", "njjjg", "nkjjg",
+                "ojjjg", "okjjg", "pjjjg", "pkjjg", "kkjjg", "kkajg", "kkakg", "kkbjg", "kkbkg",
+                "kkcjg", "kkckg", "kkdjg", "kkdkg", "kkejg", "kkekg", "kkfjg", "kkfkg", "kkgjg",
+                "kkgkg", "kkhjg", "kkhkg", "kkijg", "kkikg", "kkljg", "kklkg", "kkmjg", "kkmkg",
+                "kknjg", "kknkg", "kkojg", "kkokg", "kkpjg", "kkpkg", "kkkkg", "ggggx", "gggxx",
+                "ggxxx", "gxxxx", "xxxxx", "xxxxy", "xxxyy", "xxyyy", "xyyyy", "yyyyy", "yyyyw",
+                "yyyww", "yywww", "ywwww", "wwwww", "wwvww", "wvvww", "vvvww", "vvvwz", "avvwz",
+                "aavwz", "aaawz", "aaaaz"
+            ]
+            .into_iter()
+            .map(|w| w.to_string())
+            .collect()
+        )
+    );
+}
